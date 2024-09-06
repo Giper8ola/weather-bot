14c3cf2420e7a124329455ced0dfefdbc69f289a
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-06 16:10:59 +0300

    feat(module): implement config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TelegramModule } from './telegram/telegram.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [TelegramModule]
+    imports: [ConfigModule.forRoot({ isGlobal: true }), TelegramModule]
 })
 export class AppModule {}
