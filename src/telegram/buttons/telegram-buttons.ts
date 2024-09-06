4168df9a818651bc1249767614139c9fdc918c37
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-07 01:07:15 +0300

    fear: add location btn

--- src/telegram/buttons/telegram-buttons.ts
+++ src/telegram/buttons/telegram-buttons.ts
@@ -3,7 +3,7 @@ import { Markup } from 'telegraf';
 export function actionButtons() {
     return Markup.keyboard([
         Markup.button.callback('Выбор города', 'city'),
-        Markup.button.callback('Прогноз на 5 дней', 'weather'),
+        Markup.button.locationRequest('Погода в моем городе'),
         Markup.button.callback('Выбор единиц измерения', 'measuring')
     ]);
 }
