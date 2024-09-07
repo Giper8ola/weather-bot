55a220d926a09c4f0fb5c8d5e83afd1181859a7d
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-08 00:56:07 +0300

    feat(module): add utils module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TelegramModule } from './telegram/telegram.module';
 import { ConfigModule } from '@nestjs/config';
+import { UtilsModule } from './utils/utils.module';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), TelegramModule]
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TelegramModule,
+        UtilsModule
+    ]
 })
 export class AppModule {}
