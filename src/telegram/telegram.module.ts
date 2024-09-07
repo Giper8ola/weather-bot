2fc25102330966e609749aa55011b0e669df1461
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-08 01:38:17 +0300

    feat: implement utils to telegram

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TelegramService } from './telegram.service';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { options } from './telegraf-config.factory';
+import { UtilsModule } from '../utils/utils.module';
 
 @Module({
-    imports: [TelegrafModule.forRootAsync(options())],
+    imports: [TelegrafModule.forRootAsync(options()), UtilsModule],
     providers: [TelegramService]
 })
 export class TelegramModule {}
