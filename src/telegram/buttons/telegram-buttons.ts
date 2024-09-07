a29bd0ce99eb0331c2ef7b76a9627258873f97ca
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-07 21:27:58 +0300

    delete: safe delete temp button

--- src/telegram/buttons/telegram-buttons.ts
+++ src/telegram/buttons/telegram-buttons.ts
@@ -3,7 +3,7 @@ import { Markup } from 'telegraf';
 export function actionButtons() {
     return Markup.keyboard([
         Markup.button.callback('Выбор города', 'city'),
-        Markup.button.locationRequest('Погода в моем городе'),
-        Markup.button.callback('Выбор единиц измерения', 'measuring')
+        Markup.button.locationRequest('Погода в моем городе')
+        // Markup.button.callback('Выбор единиц измерения', 'measuring')
     ]);
 }
